872a42309d8613ad0fab66cf74190fd7fb2a00d0
ArunNejakar <arunnejakar847@gmail.com>
2025-12-27 13:28:36 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 import pytest
-from camera_manager import CameraManager, run_camera_booking
+from camera import CameraManager, run_camera_booking
 
 def test_approve():
     decision, _ = run_camera_booking(camera="Camera-A", used_hours=3, requested_hours=2, capacity=8, priority="LOW")
